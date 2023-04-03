326dbc26072d836ac513fa985f8ea3fa862c700b
RaafayAsif <Raafayasif56@gmail.com>
2023-04-04 04:14:21 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# raafayasifcalculator
+ 
---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers: {
+  numone: number;
+  numtwo: number;
+  operator: string;
+} = await inquirer.prompt([
+  {
+    type: "number",
+    name: "numone",
+    message: "ENTER 1ST NUMBER",
+  },
+  {
+    type: "number",
+    name: "numtwo",
+    message: "ENTER 2ND NUMBER",
+  },
+  {
+    type: "list",
+    name: "operator",
+    choices: ["+", "-", "*", "/"],
+    message: "SELECT WHICH OPERATION DO YOU WANT TO PERFORM",
+  },
+]);
+
+const { numone, numtwo, operator } = answers;
+
+if (numone && numtwo && operator) {
+  let result = 0;
+  if (operator === "+") {
+    result = numone + numtwo;
+  } else if (operator === "-") {
+    result = numone - numtwo;
+  } else if (operator === "*") {
+    result = numone * numtwo;
+  } else if (operator === "/") {
+    result = numone / numtwo;
+  }
+
+  console.log("YOUR ANSWER IS =", result);
+} else {
+  console.log("PLEASE ENTER VALID INPUT");
+}
